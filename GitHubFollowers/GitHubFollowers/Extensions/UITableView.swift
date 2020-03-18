8cb204f16a1c3609ac79ee2e0a3062ff4746fad6
oliwia <oliwia.michalak@gmail.com>
2020-03-18 20:04:57 +0000

    Persistence Manager, UITableView Extension & Bug Fixes, iPhoneSE Constr

---
+++ GitHubFollowers/GitHubFollowers/Extensions/UITableView.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView.swift
+//  GitHubFollowers
+//
+//  Created by Oliwia Michalak on 18/03/2020.
+//  Copyright © 2020 Oliwia Michalak. All rights reserved.
+//
+
+import Foundation
